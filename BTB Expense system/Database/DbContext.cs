e15cb5ddbf8037f94291f6d31833165419a37e43
TelmanYusifov <43857129+TelmanYusifov@users.noreply.github.com>
2019-06-16 19:32:44 +0400

    Database created. Using attributes.

--- BTB Expense system/Database/DbContext.cs
+++ BTB Expense system/Database/DbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BTB_Expense_system.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,11 @@ namespace BTB_Expense_system.Database
 {
     public class DbContext
     {
+        public DbSet<User> Users { get; set; }
 
+        public DbContext()
+        {
+            Users = new DbSet<User>();
+        }
     }
 }
--- BTB Expense system/Database/DbSet.cs
+++ BTB Expense system/Database/DbSet.cs
@@ -6,8 +6,35 @@ using System.Threading.Tasks;
 
 namespace BTB_Expense_system.Database
 {
-    public class DbSet
+    public class DbSet<T> where T:class
     {
-
+        private readonly List<T> _items;
+        public DbSet()
+        {
+            _items = new List<T>();
+        }
+        public IEnumerable<T> GetAsEnumerable()
+        {
+            foreach(T item in _items)
+            {
+                yield return item;
+            }
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _items;
+        }
+        public void Add(T Item)
+        {
+            _items.Add(Item);
+        }
+        public bool Remove (T Item)
+        {
+            return _items.Remove(Item);
+        }
+        public void RemoveAt(int index)
+        {
+            _items.RemoveAt(index);
+        }
     }
 }
